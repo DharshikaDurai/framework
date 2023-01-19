8fb2e1ea1a0fae66b1fea2bf467fd7a015ce1c04
DharshikaDurai <dharshika0895@gmail.com>
2023-01-19 18:41:32 +0530

    success

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,18 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	
+	public void saving() {
+		System.out.println("saving is 5%");
+	}
+	public void fixed() {
+		System.out.println("fixed is 2%");
+	}
+		public static void main(String[] args) {
+			AxisBank a = new AxisBank();
+			a.fixed();
+			a.saving();
+			a.deposit();
+			
+		}
+		}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,15 @@
+package org.bank;
+
+public class BankInfo{
+	public void saving() {
+		System.out.println("saving 8 %");
+	}
+	public void deposit() {
+		System.out.println("deposit 4%");
+	}
+	public void fixed() {
+		System.out.println("fixed 6%");
+			
+	}
+
+}
